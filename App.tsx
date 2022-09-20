08301a38b74f079d048fa5d81509339448cfb588
gerritt-luoma <gerritt.luoma@gmail.com>
2022-09-20 15:03:59 -0600

    Tested with 6 players

--- App.tsx
+++ App.tsx
@@ -10,6 +10,9 @@ export default function App() {
         <Player color={'red'} number={1}/>
         <Player color={'blue'} number={2}/>
         <Player color={'pink'} number={3}/>
+        <Player color={'green'} number={4}/>
+        <Player color={'white'} number={5}/>
+        <Player color={'gray'} number={6}/>
         <StatusBar style="auto" />
       </View>
     </Provider>
